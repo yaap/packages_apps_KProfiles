e6140371a340de710b5cc11f0def682ce6128062
Bruno Martins <bgcngm@gmail.com>
2025-12-14 16:46:36 +0000

    KProfiles: Convert for M3E support
    Change-Id: Ib4d1088f5be3fc2b5c2ff90d5cd1b86603551037
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/kprofiles/battery/KprofilesSettingsFragment.java
+++ src/com/android/kprofiles/battery/KprofilesSettingsFragment.java
@@ -32,13 +32,13 @@ import android.view.ViewGroup;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.kprofiles.R;
 import com.android.kprofiles.utils.FileUtils;
+import com.android.settingslib.widget.SettingsBasePreferenceFragment;
 
-public class KprofilesSettingsFragment extends PreferenceFragment implements
+public class KprofilesSettingsFragment extends SettingsBasePreferenceFragment implements
         OnPreferenceChangeListener {
 
     private SwitchPreferenceCompat kProfilesAutoPreference;
